5293d2095bb60ab62403b3680ee5bf06a8da1b6a
holisticdev <jdelaossa@russ.com.co>
2022-09-02 10:57:06 -0500

    Initial commit

---
+++ domain/src/main/java/org/example/exception/GenericSpecificationException.java
@@ -0,0 +1,8 @@
+package org.example.exception;
+
+public class GenericSpecificationException extends RuntimeException{
+
+    public GenericSpecificationException(String message) {
+        super(message);
+    }
+}
